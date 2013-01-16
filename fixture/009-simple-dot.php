c25a494cffdf88fa9dc9e53b280b314a646f7fd8
Zordius Chen <zordius@yahoo-inc.com>
2013-01-16 09:34:16 +0800

    test more on {{{raw}}} , use new lightncandy

--- fixture/009-simple-dot.php
+++ fixture/009-simple-dot.php
@@ -9,6 +9,7 @@
     );
     return 'Hello '.LightnCandy::enc('winner.name', $cx, $in).', you have just won $'.LightnCandy::enc('winner.value', $cx, $in).'!
 We have $'.LightnCandy::enc('award.first.value', $cx, $in).' for '.LightnCandy::enc('award.first.name', $cx, $in).' award!!
+Raw dot test: '.LightnCandy::enc('winner.name', $cx, $in).' '.LightnCandy::raw('award.first.value', $cx, $in).' for '.LightnCandy::raw('award.first.name', $cx, $in).'
 ';
 }
 ?>
--- fixture/012-simple-this.php
+++ fixture/012-simple-this.php
@@ -8,7 +8,7 @@
         'parents' => Array()
     );
     return 'Hello '.LightnCandy::enc('name', $cx, $in).', you have just won $'.LightnCandy::enc('value', $cx, $in).'!
-Winners: '.LightnCandy::sec('winners', $cx, $in, false, function($cx, $in) {return ''.LightnCandy::enc('', $cx, $in).'';}).'
+Winners: '.LightnCandy::sec('winners', $cx, $in, false, function($cx, $in) {return ''.LightnCandy::enc('', $cx, $in).'('.LightnCandy::raw('', $cx, $in).') = '.LightnCandy::enc('', $cx, $in).'('.LightnCandy::raw('', $cx, $in).')';}).'
 ';
 }
 ?>

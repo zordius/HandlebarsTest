de625104b89b41b6d7c961cd5c1f82df62e4d1d4
Zordius Chen <zordius@yahoo-inc.com>
2013-01-16 18:42:04 +0800

    add fixtures for #unless, use newer lightncandy

---
+++ fixture/015-hb-unless.php
@@ -0,0 +1,22 @@
+<?php return function ($in) {
+    $cx = Array(
+        'flags' => Array(
+            'jstrue' => true,
+            'jsobj' => true
+        ),
+        'path' => Array(),
+        'parents' => Array()
+    );
+    return 'Hello '.LightnCandy::enc('name', $cx, $in).', you have just won $'.LightnCandy::enc('value', $cx, $in).'!
+'.(!LightnCandy::ifvar('test', $in) ? ('
+Yes! '.LightnCandy::enc('name', $cx, $in).' is '.LightnCandy::enc('gender', $cx, $in).'
+') : '').'
+'.(!LightnCandy::ifvar('test', $in) ? ('
+2nd If, '.LightnCandy::enc('name', $cx, $in).' is '.LightnCandy::raw('gender', $cx, $in).'
+') : ('
+Else test, '.LightnCandy::enc('name', $cx, $in).' is '.LightnCandy::enc('gender', $cx, $in).'
+')).'
+'.LightnCandy::enc('end', $cx, $in).'
+';
+}
+?>

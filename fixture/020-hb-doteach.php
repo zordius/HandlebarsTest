652e24fea01e5e182fe6ca3fd50dad0a66efe11b
Zordius Chen <zordius@yahoo-inc.com>
2013-05-22 20:07:26 +0800

    add 020-hb-doteach test case, use new lightncandy which is fixed for this bug

---
+++ fixture/020-hb-doteach.php
@@ -0,0 +1,23 @@
+<?php return function ($in) {
+    $cx = Array(
+        'flags' => Array(
+            'jstrue' => true,
+            'jsobj' => true
+        ),
+        'path' => Array(),
+        'parents' => Array()
+    );
+    return 'Hello '.LCRun::enc('name', $cx, $in).', you have just won $'.LCRun::enc('value', $cx, $in).'!
+<ul>
+'.LCRun::sec('people.boss', $cx, $in, true, function($cx, $in) {return '
+ <li>'.LCRun::enc('name', $cx, $in).' is a '.LCRun::enc('gender', $cx, $in).' ('.LCRun::enc('name', $cx, $in).', '.LCRun::enc('value', $cx, $in).', '.LCRun::enc('end', $cx, $in).')</li>
+';}).'
+</ul>
+'.LCRun::enc('end', $cx, $in).'
+'.LCRun::sec('people.boss', $cx, $in, false, function($cx, $in) {return '
+ THIS:'.LCRun::enc('name', $cx, $in).' is a '.LCRun::raw('gender', $cx, $in).'
+ PARENT: '.LCRun::raw('name', $cx, $in).', '.LCRun::raw('value', $cx, $in).', '.LCRun::raw('end', $cx, $in).'
+';}).'
+';
+}
+?>

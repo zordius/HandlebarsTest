180c85391dfe14290c90aa8e024a5c3b814eef70
Zordius Chen <zordius@yahoo-inc.com>
2013-05-23 11:42:53 +0800

    use new lightncandy fixed {{#.}} and {{#this}} bug

---
+++ fixture/021-hb-manyparent.php
@@ -0,0 +1,64 @@
+<?php return function ($in) {
+    $cx = Array(
+        'flags' => Array(
+            'jstrue' => true,
+            'jsobj' => true,
+        ),
+        'scopes' => Array(),
+        'path' => Array(),
+        'parents' => Array()
+    );
+    return 'Hello '.LCRun::enc('name', $cx, $in).', you have just won $'.LCRun::enc('value', $cx, $in).'!
+<ul>
+'.LCRun::sec('', $cx, $in, true, function($cx, $in) {return '
+ <li>'.LCRun::enc('name', $cx, $in).' is a '.LCRun::enc('gender', $cx, $in).' ('.LCRun::enc('../name', $cx, $in).', '.LCRun::enc('../value', $cx, $in).', '.LCRun::enc('../end', $cx, $in).')</li>
+ '.(LCRun::ifvar('good', $cx, $in) ? ('
+  GOOD! '.LCRun::enc('goodchild', $cx, $in).' '.LCRun::enc('../gender', $cx, $in).' - '.LCRun::raw('../gender', $cx, $in).' X '.LCRun::enc('../../name', $cx, $in).' ~ '.LCRun::enc('../../end', $cx, $in).'
+ ') : ('
+  BAD! '.LCRun::enc('badchild', $cx, $in).' '.LCRun::enc('../gender', $cx, $in).' - '.LCRun::raw('../gender', $cx, $in).' Y '.LCRun::raw('../../name', $cx, $in).' = '.LCRun::raw('../../end', $cx, $in).'
+ ')).'
+ '.LCRun::wi('good', $cx, $in, function($cx, $in) {return '
+    WITH: '.LCRun::enc('../gender', $cx, $in).' , '.LCRun::enc('../../name', $cx, $in).' , '.LCRun::raw('../../end', $cx, $in).'
+ ';}).'
+';}).'
+----------THIS
+'.LCRun::sec('', $cx, $in, true, function($cx, $in) {return '
+ <li>'.LCRun::enc('name', $cx, $in).' is a '.LCRun::enc('gender', $cx, $in).' ('.LCRun::enc('../name', $cx, $in).', '.LCRun::enc('../value', $cx, $in).', '.LCRun::enc('../end', $cx, $in).')</li>
+ '.(LCRun::ifvar('good', $cx, $in) ? ('
+  GOOD! '.LCRun::enc('goodchild', $cx, $in).' '.LCRun::enc('../gender', $cx, $in).' - '.LCRun::raw('../gender', $cx, $in).' X '.LCRun::enc('../../name', $cx, $in).' ~ '.LCRun::enc('../../end', $cx, $in).'
+ ') : ('
+  BAD! '.LCRun::enc('badchild', $cx, $in).' '.LCRun::enc('../gender', $cx, $in).' - '.LCRun::raw('../gender', $cx, $in).' Y '.LCRun::raw('../../name', $cx, $in).' = '.LCRun::raw('../../end', $cx, $in).'
+ ')).'
+ '.LCRun::wi('good', $cx, $in, function($cx, $in) {return '
+    WITH: '.LCRun::enc('../gender', $cx, $in).' , '.LCRun::enc('../../name', $cx, $in).' , '.LCRun::raw('../../end', $cx, $in).'
+ ';}).'
+';}).'
+----------SECTION THIS
+'.LCRun::sec('', $cx, $in, false, function($cx, $in) {return '
+ <li>'.LCRun::enc('name', $cx, $in).' is a '.LCRun::enc('gender', $cx, $in).' ('.LCRun::enc('../name', $cx, $in).', '.LCRun::enc('../value', $cx, $in).', '.LCRun::enc('../end', $cx, $in).')</li>
+ '.(LCRun::ifvar('good', $cx, $in) ? ('
+  GOOD! '.LCRun::enc('goodchild', $cx, $in).' '.LCRun::enc('../gender', $cx, $in).' - '.LCRun::raw('../gender', $cx, $in).' X '.LCRun::enc('../../name', $cx, $in).' ~ '.LCRun::enc('../../end', $cx, $in).'
+ ') : ('
+  BAD! '.LCRun::enc('badchild', $cx, $in).' '.LCRun::enc('../gender', $cx, $in).' - '.LCRun::raw('../gender', $cx, $in).' Y '.LCRun::raw('../../name', $cx, $in).' = '.LCRun::raw('../../end', $cx, $in).'
+ ')).'
+ '.LCRun::wi('good', $cx, $in, function($cx, $in) {return '
+    WITH: '.LCRun::enc('../gender', $cx, $in).' , '.LCRun::enc('../../name', $cx, $in).' , '.LCRun::raw('../../end', $cx, $in).'
+ ';}).'
+';}).'
+----------SECTION .
+'.LCRun::sec('', $cx, $in, false, function($cx, $in) {return '
+ <li>'.LCRun::enc('name', $cx, $in).' is a '.LCRun::enc('gender', $cx, $in).' ('.LCRun::enc('../name', $cx, $in).', '.LCRun::enc('../value', $cx, $in).', '.LCRun::enc('../end', $cx, $in).')</li>
+ '.(LCRun::ifvar('good', $cx, $in) ? ('
+  GOOD! '.LCRun::enc('goodchild', $cx, $in).' '.LCRun::enc('../gender', $cx, $in).' - '.LCRun::raw('../gender', $cx, $in).' X '.LCRun::enc('../../name', $cx, $in).' ~ '.LCRun::enc('../../end', $cx, $in).'
+ ') : ('
+  BAD! '.LCRun::enc('badchild', $cx, $in).' '.LCRun::enc('../gender', $cx, $in).' - '.LCRun::raw('../gender', $cx, $in).' Y '.LCRun::raw('../../name', $cx, $in).' = '.LCRun::raw('../../end', $cx, $in).'
+ ')).'
+ '.LCRun::wi('good', $cx, $in, function($cx, $in) {return '
+    WITH: '.LCRun::enc('../gender', $cx, $in).' , '.LCRun::enc('../../name', $cx, $in).' , '.LCRun::raw('../../end', $cx, $in).'
+ ';}).'
+';}).'
+</ul>
+'.LCRun::enc('end', $cx, $in).'
+';
+}
+?>

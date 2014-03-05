397cb4fe1e4e38bf677950e733409eb001b43cae
Zordius Chen <zordius@yahoo-inc.com>
2014-03-05 15:15:26 +0800

    add new tests for named arguments

---
+++ fixture/030-hb-namedarg.php
@@ -0,0 +1,27 @@
+<?php return function ($in) {
+    $cx = Array(
+        'flags' => Array(
+            'jstrue' => true,
+            'jsobj' => true,
+        ),
+        'helpers' => Array(            'helper1' => function($url, $txt) {
+                $u = ($url !== null) ? $url : 'undefined';
+                $t = ($txt !== null) ? $txt : 'undefined';
+                return "<a href=\"{$u}\">{$t}</a>";
+            },
+),
+        'scopes' => Array($in),
+        'path' => Array(),
+
+    );
+    return 'Hello '.LCRun2::encq(Array('name'), $cx, $in).', you have just won $'.LCRun2::encq(Array('value'), $cx, $in).'!
+
+. Test 1: '.LCRun2::ch('helper1', Array(Array('url'),Array('"this is a test & OK"')), 'raw', $cx, $in).' !!
+. Test 2: '.LCRun2::ch('helper1', Array(Array('url'),Array('"this is a test"')), 'raw', $cx, $in).' !!
+. Test 3: '.LCRun2::ch('helper1', Array(Array('url'),Array('"this is a test & OK"')), 'encq', $cx, $in).' !!
+. Test 4: '.LCRun2::ch('helper1', Array(Array('url'),Array('"this is a test"')), 'encq', $cx, $in).' !!
+. Test 5: '.LCRun2::ch('helper1', Array(Array('url'),Array('"this is a test"')), 'encq', $cx, $in).' !!
+. Test 6: '.LCRun2::ch('helper1', Array(Array('url'),Array('"this is a test"')), 'encq', $cx, $in).' !!
+';
+}
+?>

fc4c9a01bf97ca05b492c27c27544d7c9e9c1ac4
Zordius Chen <zordius@yahoo-inc.com>
2014-07-31 17:05:37 +0800

    try to save features.png

---
+++ bin/save-features.js
@@ -0,0 +1,8 @@
+var page = require('webpage').create(),
+    system = require('system');
+
+page.viewportSize = {width: 720, height: 5500};
+page.open('index.html#!FEATURES.md', function (status) {
+    page.render('features.png');
+    phantom.exit();
+});

ada96bd095328fea8f0ce388255ea481fcc17bce
Zordius Chen <zordius@yahoo-inc.com>
2014-07-31 21:39:07 +0800

    make png file small

--- bin/save-features.js
+++ bin/save-features.js
@@ -3,6 +3,6 @@ var page = require('webpage').create(),
 
 page.viewportSize = {width: 720, height: 5500};
 page.open('index.html#!FEATURES.md', function (status) {
-    page.render('features.png');
+    page.render('features0.png');
     phantom.exit();
 });

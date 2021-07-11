606763119d820a2550f028d2d6d075cd36870e2f
Zordius <1201409+zordius@users.noreply.github.com>
2021-07-11 20:56:09 +0900

    chore: try to fix chart preview

--- bin/htmlToPng.js
+++ bin/htmlToPng.js
@@ -6,23 +6,27 @@ const path = process.cwd()
 app.use(express.static(path))
 const server = app.listen(3000)
 
-const file = process.argv[2]
-const width = 1 * (process.argv[3] || 1024)
-const height = 1 * (process.argv[4] || 768)
+if (!process.argv[2] === 'server') {
+  const file = process.argv[2]
+  const width = 1 * (process.argv[3] || 1024)
+  const height = 1 * (process.argv[4] || 768)
 
-console.log(file, width, height)
+  console.log(file, width, height)
 
-const job = async () => {
-  const browser = await puppeteer.launch()
-  const page = await browser.newPage()
+  const job = async () => {
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
 
-  page.on('console', msg => console.log(msg))
+    page.on('console', msg => console.log(msg))
+    page.on('error', err => console.error(err))
 
-  page.setViewport({ width, height })
-  await page.goto(`http://localhost:3000/${file}`)
-  await page.screenshot({path: 'screenshot.png'})
-  await browser.close()
-  server.close()
+    page.setViewport({ width, height })
+    await page.goto(`http://localhost:3000/${file}`)
+    page.waitForTimeout(5000)
+    await page.screenshot({path: 'screenshot.png'})
+    await browser.close()
+    server.close()
+  }
+
+  job()
 }
-
-job()
--- bin/save-chart.js
+++
@@ -1,8 +0,0 @@
-var page = require('webpage').create(),
-    system = require('system');
-
-page.viewportSize = {width: 800, height: 600};
-page.open(system.args[1], function (status) {
-    page.render('chart.png');
-    phantom.exit();
-});
